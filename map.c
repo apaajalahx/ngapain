c298124eb02bab6c0b053ff80ae937fbbd2f2406
Dinar Hamid <52324987+apaajalahx@users.noreply.github.com>
2024-05-19 12:59:31 +0700

    Update map.c

--- map.c
+++ map.c
@@ -10,82 +10,79 @@ struct Map{
 
 void push(struct Map *map, char key[], char value[]);
 
+void free_map(struct Map *map);
+
 int main() {
     struct Map map;
     map.sizeOfMaps = 0;
-    push(&map, "abc","def");
+    map.keys = NULL; // inisialisasi ke NULL untuk menghindari error.
+    map.values = NULL; // inisialisasi ke NULL untuk menghindari error.
+    
+    push(&map, "firstName","dinar");
+    push(&map, "lastName", "hamid");
 
     for(size_t i = 0; i < map.sizeOfMaps; ++i) {
-        printf("%s\n", map.keys[i]);
-        printf("%s\n", map.values[i]);
-        printf("%p\n", &map.keys);
-        printf("%p\n", &map.keys[i][0]);
-        printf("%d\n", i);
-        free(map.keys[i]);
-        free(map.values[i]);
+        printf("Key : %s, Value: %s\n", map.keys[i], map.values[i]);
     }
 
-    free(map.keys);
-    free(map.values);
-    
-    map.sizeOfMaps = 0;
-    
+    free_map(&map);
+
+    if (map.keys == NULL && map.values == NULL) {
+        printf("Memori berhasil di bebaskan");
+    }
+
     return 0;
 }
 
 void push(struct Map *map, char key[], char value[]) {
     
-    int size = map->sizeOfMaps;
-
-    // check apakah size lebih besar dari 0, jika lebih besar maka lakukan alokasi ulang, jika tidak maka buat alokasi baru.
-    if (size > 0) {
-
-        size += 1;
+    size_t size = map->sizeOfMaps + 1;
         
-        // alokasi ulang memori
-        map->keys = realloc(map->keys, (map->sizeOfMaps + 1) * sizeof(char));
-        if (map->keys == NULL) {
-            printf("Gagal alokasi ulang memori pada key");
-            exit(1);
-        }
+    // alokasi ulang memori
+    map->keys = realloc(map->keys, size * sizeof(char *));
+    if (map->keys == NULL) {
+        printf("Gagal alokasi ulang memori pada key");
+        exit(1);
+    }
 
-        map->values = realloc(map->values, (map->sizeOfMaps + 1) * sizeof(char));
-        if (map->values == NULL) {
-            printf("Gagal alokasi ulang memor pada value");
-            exit(1);
-        }
-    } else {
-        
-        // alokasi memory
-        map->keys = calloc(1, sizeof(char));
-        if (map->keys == NULL) {
-            printf("Gagal Alokasi memori pada key");
-            exit(1);
-        }
-
-        map->values = calloc(1, sizeof(char));
-        if (map->values == NULL) {
-            printf("Gagal Alokasi memori pada value");
-            exit(1);
-        }
-
+    map->values = realloc(map->values, size * sizeof(char *));
+    if (map->values == NULL) {
+        printf("Gagal alokasi ulang memor pada value");
+        exit(1);
     }
 
     // alokasi size memori pada array key value
-    map->keys[size] = malloc(strlen(key) * sizeof(char));
-    if (map->keys[size] == NULL) {
+    map->keys[map->sizeOfMaps] = malloc((strlen(key) + 1) * sizeof(char));
+    if (map->keys[map->sizeOfMaps] == NULL) {
         printf("Gagal Alokasi memori pada key");
         exit(1);
     }
 
     // alokasi size memori pada array key value
-    map->values[size] = malloc(strlen(value) * sizeof(char));
-    if (map->values[size] == NULL) {
+    map->values[map->sizeOfMaps] = malloc((strlen(value) + 1) * sizeof(char));
+    if (map->values[map->sizeOfMaps] == NULL) {
         printf("Gagal Alokasi memori pada value");
         exit(1);
     }
 
-    strcpy(map->keys[size], key);
-    strcpy(map->values[size], value);
+    strcpy(map->keys[map->sizeOfMaps], key);
+    strcpy(map->values[map->sizeOfMaps], value);
     map->sizeOfMaps += 1;
+}
+
+
+void free_map(struct Map *map) {
+    for (size_t i = 0; i < map->sizeOfMaps; ++i) {
+        free(map->keys[i]);
+        map->keys[i] = NULL;
+        free(map->values[i]);
+        map->keys[i] = NULL;
+    }
+
+    free(map->keys);
+    free(map->values);
+
+    map->sizeOfMaps = 0;
+    map->keys = NULL;
+    map->values = NULL;
 }
